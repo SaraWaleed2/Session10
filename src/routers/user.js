a2ddfbe7fc935fc3b9b58e1cb13c50efbcba01a3
Sara <sarawaleed8479@gmail.com>
2023-12-23 21:34:56 +0200

    first commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,10 +7,10 @@ router.post("/users", (req, res) => {
     const user = new User(req.body)
     user.save()
         .then((user) => {
-            res.send(user);
+            res.status(200).send(user);
         })
         .catch((e) => {
-            res.send(e);
+            res.status(400).send(e);
         })
 })
 
